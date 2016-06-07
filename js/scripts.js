a4a085dd4df2dc9bc2356a98f1b75c4f5d968de3
Caleb Stokka and Sky Rousse <caleb@>
2016-06-07 09:33:11 -0700

    add ability to handle negative integers with Math.abs

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ $(function() {
   event.preventDefault();
 
     var total = 0;
-    var limit = parseInt($("input#limit").val());
-    var increment = parseInt($("input#increment").val());
+    var limit = Math.abs(parseInt($("input#limit").val()));
+    var increment = Math.abs(parseInt($("input#increment").val()));
     for (var currentNumber = increment; currentNumber <= limit; currentNumber += increment) {
       total += increment;
       $("ul#output").append("<li>" + "Total is: " + total + "</li>");
